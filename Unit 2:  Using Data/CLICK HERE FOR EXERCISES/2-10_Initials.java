b51968a2e53c7bdb754ffe5a1d9a4c6e0d328f5b
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2024-10-12 17:18:14 -0500

    Create 2-10_Initials.java

---
+++ Unit 2:  Using Data/CLICK HERE FOR EXERCISES/2-10_Initials.java
@@ -0,0 +1,14 @@
+/*
+    SirSpooki
+    SlayTeam 2023
+*/
+class Initials {
+    public static void main(String[] args) 
+    {
+        char init1 = 'J';
+        char init2 = 'M';
+        char init3 = 'F';
+
+        System.out.println(init1 +"."+init2+"."+init3+".");
+    }
+}
