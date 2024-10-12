1fcd0a27e9dee8cfd086c14f8a7fef5ff69b6cc8
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2024-10-12 17:09:41 -0500

    Update DebugOne3.java

---
+++ Unit 1:  Creating Java Programs/CLICK HERE FOR DEBUGGING/DebugOne3.java
@@ -0,0 +1,13 @@
+/*
+    SirSpooki
+    SlayTeam 2023
+*/
+public class DebugOne3
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Over the river");
+      System.out.println("and through the woods");
+      System.out.println("to Grandmother's house we go");
+   }
+}
